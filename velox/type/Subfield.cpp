08c514050e12bbbfa67ab20b2d5a0ecd5da87d18
David Martinez <david.martinez.garcia@intel.com>
2024-01-05 16:35:02 -0600

    Failing with new variadic macros

--- velox/type/Subfield.cpp
+++ velox/type/Subfield.cpp
@@ -39,7 +39,7 @@ Subfield::Subfield(
 
 Subfield::Subfield(std::vector<std::unique_ptr<Subfield::PathElement>>&& path)
     : path_(std::move(path)) {
-  VELOX_CHECK_GE(path_.size(), 1);
+  VELOX_CHECK_GE_W(path_.size(), 1);
 };
 
 Subfield Subfield::clone() const {
